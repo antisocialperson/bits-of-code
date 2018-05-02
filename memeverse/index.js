008694eae19734868f9fee3bcfc9d058e5918370
Asdf Ghjkl <34778265+antisocialperson@users.noreply.github.com>
2018-05-01 19:50:52 -0700

    delete

--- memeverse/index.js
+++
@@ -1,6 +0,0 @@
-function hi() { 
-    document.getElementById("demo").innerHTML = "I love tpuncil";
-}
-function steamramsam() { 
-    document.getElementById("demo").innerHTML = "Hi. My name is Albert and I go to Redwood Middle School. I do not like Google Sites and I do like HTML/CSS/JS. However, I like Java and C++ more. <br><br> My AoPS username is Welp... because I typed in the first phrase that came to my mind.";
-}
